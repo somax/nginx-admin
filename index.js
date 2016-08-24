305fcd9152103d7d1c51907ba16b686a807eee22
somax <somaxj@163.com>
2016-08-24 22:18:04 +0800

    refactory: parsePath,parseBody,content-type:json

--- index.js
+++ index.js
@@ -1,50 +1,95 @@
 const fs = require('fs');
-const path = require('path');
+const Path = require('path');
 const qs = require('querystring');
 const http = require('http');
 const exec = require('child_process').execFile;
 
-const VERSION =  require('./package.json').version;
+const VERSION = require('./package.json').version;
 const CONFIG = require('ez-config-loader')('config/config');
 
 const PORT = CONFIG.port || 8000;
 const CONFIG_PATH = prefixPath(CONFIG.configPath);
 const NGINX_CMD = CONFIG.nginxCmd;
 
-console.log('Config loaded:\n',CONFIG);
+console.log('Config loaded:\n', CONFIG);
 
 function prefixPath(_path) {
-    return (/^\/./.test( _path)) ? _path : __dirname + '/' + _path + '/';
+    return (/^\/./.test(_path)) ? _path : __dirname + '/' + _path + '/';
+}
+
+function parsePath(req) {
+    req.$path = Path.parse(req.url, true);
+}
+
+function parseBody(req) {
+    const defer = Promise.defer();
+
+    if(req.method === 'GET'){
+        defer.resolve();
+    }
+
+    const contentType = req.headers['content-type'] || '*';
+    const parseFun = {
+        'application/json': data => JSON.parse(data),
+        'application/x-www-form-urlencoded': data => qs.parse(data),
+        '*': data => data
+    };
+
+    let body = '';
+
+    req.on('data', function(data) {
+        body += data;
+
+        if (body.length > 1e6) {
+            req.connection.destroy();
+        }
+    });
+
+    req.on('end', function() {
+        try {
+            req.$body = parseFun[contentType](body);
+            defer.resolve(req.$body);
+        } catch (err) {
+            defer.reject(err);
+        }
+
+    });
+
+    req.on('error', (err) => {
+        defer.reject(err);
+    });
+
+    return defer.promise;
 }
 
 const nginxCommands = {
-    'reload':(cb)=>{
+    'reload': (cb) => {
         console.log('do nginx reload!');
-        exec(NGINX_CMD,['-s','reload'],cb);
+        exec(NGINX_CMD, ['-s', 'reload'], cb);
         // console.log(exec(NGINX_CMD,['-s','reloadd']).toString());
         // cb(false,'done');
     }
 };
 
 const apiHandler = {
-    '/':{
-        'GET':(req, done)=>{
+    '/': {
+        'GET': (req, done) => {
             done({
-                info:`Nginx Admin API! Version: ${VERSION}`,
-                apis:['/api/conf']
+                info: `Nginx Admin API! Version: ${VERSION}`,
+                apis: ['/api/conf']
             });
         }
     },
-    '/api':{
-        'GET':(req, done)=>{
+    '/api': {
+        'GET': (req, done) => {
             let _res;
-            let _path = req._path;
-            if(_path.base === 'conf'){
+            let _path = req.$path;
+            if (_path.base === 'conf') {
                 _res = {
-                    apis : '/api/conf/vhosts'
+                    apis: '/api/conf/vhosts'
                 };
-            }else{
-                _res = { 
+            } else {
+                _res = {
                     statusCode: 404,
                     msg: 'not found!'
                 };
@@ -52,59 +97,52 @@ const apiHandler = {
             done(_res);
         }
     },
-    '/api/conf':{
-        'GET': (req, done)=>{
-            let _path = req._path;
-            let _files = fs.readdirSync( CONFIG_PATH + _path.base);
+    '/api/conf': {
+        'GET': (req, done) => {
+            let _path = req.$path;
+            let _files = fs.readdirSync(CONFIG_PATH + _path.base);
             done({
-                apis: _files.map(v=>`/api/conf/${_path.base}/${v}`)
+                apis: _files.map(v => `/api/conf/${_path.base}/${v}`)
             });
         }
     },
-    '/api/conf/vhosts':{
-        'GET': (req, done)=>{
-            let _path = req._path;
+    '/api/conf/vhosts': {
+        'GET': (req, done) => {
+            let _confPath = CONFIG_PATH + 'vhosts/' + req.$path.base;
             done({
-                data: fs.readFileSync( CONFIG_PATH + '/vhosts/' + _path.base).toString()
+                conf: fs.readFileSync(_confPath).toString(),
+                file: _confPath
             });
         },
-        'POST': (req, done)=>{
-            let body = '';
-            let _path = req._path;
-
-            req.on('data', function (data) {
-                body += data;
-
-                if (body.length > 1e6){
-                    req.connection.destroy();
-                }
+        'POST': (req, done) => {
+            let _confPath = CONFIG_PATH + 'vhosts/' + req.$path.base;
+            fs.writeFileSync(_confPath, req.$body['conf']);
+            done({
+                msg: 'config file saved',
+                file: _confPath
             });
-
-            req.on('end', function () {
-                let post = qs.parse(body);
-                fs.writeFileSync(CONFIG_PATH + '/vhosts/' + _path.base, post['conf']);
-                done({ msg: 'ok' });
-            });
-
         },
-        'DELETE':(req, done)=>{
-            let _path = req._path;
-            fs.unlinkSync(CONFIG_PATH + _path.base);
-            done({msg: 'deleted'});
+        'DELETE': (req, done) => {
+            fs.unlinkSync(CONFIG_PATH + 'vhosts/' + req.$path.base);
+            done({
+                msg: 'deleted'
+            });
         }
     },
-    '/api/cmd':{
-        'GET':(req, done)=>{
-            let _path = req._path;
+    '/api/cmd': {
+        'GET': (req, done) => {
+            let _path = req.$path;
             let _cmd = _path.base;
-            nginxCommands[_cmd]((err,_stdout,_stderr)=>{
-                let _res = { cmd: _cmd };
-                if(err){
+            nginxCommands[_cmd]((err, _stdout, _stderr) => {
+                let _res = {
+                    cmd: _cmd
+                };
+                if (err) {
                     _res.error = err;
                 }
                 _res.stdout = _stdout;
                 _res.stderr = _stderr;
-                done(_res);                
+                done(_res);
             });
 
         }
@@ -114,29 +152,39 @@ const apiHandler = {
 
 
 
-// apiHandler['/api/conf/vhosts'].PUT = apiHandler['/api/conf/vhosts'].POST;
-
 const httpServer = http.createServer((req, res) => {
 
-    let _path = path.parse(req.url, true);
-    req._path = _path;
-    console.log(_path);
 
-    if(/^\/api/.test(req.url)){
-        try{
+    if (/^\/api/.test(req.url)) {
 
-            apiHandler[_path.dir][req.method](req, (_res={}) => {
-                if(_res.statusCode){
-                    res.statusCode = _res.statusCode;
-                }
-                res.end(JSON.stringify(_res));
-            });
-        }catch(err){
+        parsePath(req);
+
+        parseBody(req).then(
+        (body) => {
+            try {
+                apiHandler[req.$path.dir][req.method](req, (_res = {}) => {
+                    if (_res.statusCode) {
+                        res.statusCode = _res.statusCode;
+                    }
+                    res.setHeader('content-type','application/json');
+                    res.end(JSON.stringify(_res));
+                });
+            } catch (err) {
+                console.error(err);
+                res.statusCode = 500;
+                res.end(JSON.stringify({
+                    msg: err.toString()
+                }));
+            }
+        },
+        (err) => {
             console.error(err);
             res.statusCode = 500;
-            res.end(JSON.stringify({ msg: err.toString()}));
-        }
-    }else{
+            res.end(JSON.stringify({
+                msg: err.toString()
+            }));
+        });
+    } else {
         let file;
         let url = (req.url == '/') ? './index.html' : '.' + req.url;
         try {
@@ -145,7 +193,7 @@ const httpServer = http.createServer((req, res) => {
             file = 'Not find: ' + url;
             res.statusCode = 404;
         }
-        res.end(file);        
+        res.end(file);
     }
 
 
